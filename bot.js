48b255b04808b5ecfddcc246e4bdfd65ef43f209
ilovewolf <39267590+ilovewolf@users.noreply.github.com>
2018-07-01 20:17:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ return;
 
 case "help":
 try {
-message.channel.send(`= COMMANDS =\n\n$minecraft - Generate an Minecraft premium account.\n$spotify - Generate an Spotify premium account.\n$uplay - Generate an UPlay account with games!\n$origin - Generate an origin account with games!\n\nThis bot was created by ${bot.users.get("429199866657243146").username}#${bot.users.get("429199866657243146").discriminator}\n${bot.users.get("425184699053244416").username}#${bot.users.get("425184699053244416").discriminator} - refiller\n${bot.users.get("279735047366377472").username}#${bot.users.get("279735047366377472").discriminator} - refiller.`, {code: "asciidoc"});
+message.channel.send(`= COMMANDS =\n\n$minecraft - Generate an Minecraft premium account.\n$spotify - Generate an Spotify premium account.\n$uplay - Generate an UPlay account with games!\n$origin - Generate an origin account with games!\n\nThis bot has been developed by ${bot.users.get("463024524263161877").username}#${bot.users.get("463024524263161877").discriminator}\n${bot.users.get("425184699053244416").username}#${bot.users.get("425184699053244416").discriminator} - refiller\n${bot.users.get("279735047366377472").username}#${bot.users.get("279735047366377472").discriminator} - refiller.`, {code: "asciidoc"});
 } catch(err) {
 	console.log(err);
 	return;
@@ -80,7 +80,7 @@ message.channel.send(`= COMMANDS =\n\n$minecraft - Generate an Minecraft premium
 break;
 
 case "eval":
-    if(message.author.id !== "429199866657243146") return;
+    if(message.author.id !== "463024524263161877") return;
     try {
       const code = args.join(" ").slice(4);
       let evaled = eval(code);
