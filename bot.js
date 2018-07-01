ae558a0128153c332899893216ac7a9d0ea172a3
ilovewolf <39267590+ilovewolf@users.noreply.github.com>
2018-07-01 17:57:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,88 +1,80 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+var weather = require("weather-js");
+const bot = new Discord.Client({autoReconnect: true});
+const moment = require('moment');
+const economy = require('discord-eco');
+const cheerio = require('cheerio'),
+      snekfetch = require('snekfetch'),
+      querystring = require('querystring');
 var talkedRecently = new Set();
 //
 var fs = require("fs");
 var text = fs.readFileSync("./minecraft.txt").toString('utf-8');
 var textByLine = text.split("\n")
+// blacklist code jos:
+let bltext = fs.readFileSync("./blacklist.txt").toString('utf-8');
+let blarray = bltext.split("\n");
+//
 const items = JSON.parse(fs.readFileSync('items.json', 'utf8'));
+// uplay
+let uplaytext = fs.readFileSync("./uplay.txt").toString('utf-8');
+let uplayarray = uplaytext.split("\n");
+//
+// origin
+let origintext = fs.readFileSync("./origin.txt").toString('utf-8');
+let originarray = origintext.split("\n");
+//
 // spotify
 var fs = require("fs");
 var text = fs.readFileSync("./spotify.txt").toString('utf-8');
 var alt = text.split("\n")
 //
+var getgeoip = require('ip-geoinfo');
 var fs = require("fs");
 var requireText = require('require-text');
 let c = "#ff0000";
-const ids = ["429199866657243146", "279735047366377472", "425184699053244416"];
-const clean = text => {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
-let prefix = "$";
+let prefix = "!";
 bot.on("ready", () => {
 	console.log(`Logged in as ${bot.user.tag}, ${bot.user.id}`);
 	bot.user.setStatus("online");
-    bot.user.setActivity(`with my dad`, {
-    url: "https://twitch.tv/wolfyyxd",
-	  type: "Streaming"
+    bot.user.setActivity(`$help`, {
+	  type: "Watching"
 	});
 });
 
-bot.on("guildCreate", guild => {
-  let embed = new Discord.RichEmbed()
-  .setAuthor(bot.user.tag, bot.user.avatarURL)
-  .setTitle("I joined " + guild.name)
-  .addField("Members", guild.members.size, false)
-  .addField("ID", guild.id, false)
-  .addField("Owner", guild.owner.user.username, false)
-  .setTimestamp()
-  .setColor(guild.members.get(bot.user.id).displayHexColor)
-  .setFooter("Guild count: " + bot.guilds.size, bot.user.avatarURL)
-  bot.channels.get("461558684045148160").send({embed})
-});
-
 bot.on("message", message => {
-
 if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+var giiid = "402499256591712257";
     if(!message.content.startsWith(prefix)) return;
-
     var args = message.content.substring(prefix.length).split(" ");
     switch (args[0].toLowerCase()) {
 
 // comenzi normale
 
-case "leave":
+case "say":
+if(message.guild.id !== "462635331712319498") return;
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No permission");
 try {
-  if(message.author.id.includes(ids)) return message.channel.send("You're not allowed to use this.");
-  message.channel.send("**Well I'll leave now, bye!!**");
-  bot.guilds.get(message.guild.id).leave();
-} catch(err) {
-  message.channel.send("ERROR");
-  return;
+message.channel.send(args.join(" ").slice(3));
+message.delete();
 }
-break;
-
-case "eval":
-if(message.author.id != "429199866657243146") return message.channel.send(":octagonal_sign: **Only my developer can access this command.**");
-try {
-  const code = args.join(" ").slice(4);
-  let evaled = eval(code);
-
-  if (typeof evaled !== "string")
-    evaled = require("util").inspect(evaled);
-
-  message.channel.send(clean(evaled), {code:"xl"});
-} catch (err) {
-  message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
+catch(err) {
+message.channel.send(`\`\`\`${err}\`\`\``);
 }
 return;
 
-case "minecraft":
-if(message.guild.id == "431887036178497546") return message.channel.send("You cant use this bot on this server!");
+case "help":
 try {
+message.channel.send(`= COMMANDS =\n\n$minecraft - Generate an Minecraft premium account.\n$spotify - Generate an Spotify premium account.\n$uplay - Generate an UPlay account with games!\n$origin - Generate an origin account with games!\n\nThis bot was created by ${bot.users.get("429199866657243146").username}#${bot.users.get("429199866657243146").discriminator}.`, {code: "asciidoc"}
+} catch(err) {
+	console.log(err);
+	return;
+}
+break;
+
+case "minecraft":
+if(message.guild.id === "462635331712319498") return;
 if(talkedRecently.has(message.author.id)) {
 message.channel.send(`${message.author.tag}, You need to wait at least 5 minutes to type !minecraft again!`);
   return;
@@ -92,24 +84,70 @@ setTimeout(() => {
   talkedRecently.delete(message.author.id);
 }, 300000);
 var randomalt = textByLine[Math.floor(Math.random() * textByLine.length)];
-message.author.send("Keep in mind that, we use ads to purchase new accounts \:)");
 var embed = new Discord.RichEmbed()
-.setTitle(`Here is your Minecraft account.`)
+.setTitle(`Here is ur minecraft alt, sir.`)
 .setDescription(randomalt)
 .setColor(c)
-.setTimestamp()
-.setFooter("Thanks for using our Public GEN!", message.author.avatarURL)
-message.author.send({embed});
+message.author.sendMessage({embed});
 message.reply("check DMs.");
-} catch(err) {
-  message.channel.send("You have DMs locked so I can't message you.");
-  return;
-}
 break;
 
+case "uplay":
+if(message.guild.id === "462635331712319498") return;
+if(talkedRecently.has(message.author.id)) {
+message.channel.send(`${message.author.tag}, You need to wait at least 5 minutes to type !minecraft again!`);
+  return;
+	};
+talkedRecently.add(message.author.id);
+setTimeout(() => {
+  talkedRecently.delete(message.author.id);
+}, 300000);
+var uplayalt = uplayarray[Math.floor(Math.random() * uplayarray.length)];
+var embed = new Discord.RichEmbed()
+.setTitle(`Here is ur UPlay account, sir.`)
+.setDescription(uplayalt)
+.setColor(c)
+message.author.sendMessage({embed});
+message.reply("check DMs.");
+break;
+
+case "origin":
+if(message.guild.id === "462635331712319498") return;
+if(talkedRecently.has(message.author.id)) {
+message.channel.send(`${message.author.tag}, You need to wait at least 5 minutes to type !minecraft again!`);
+  return;
+	};
+talkedRecently.add(message.author.id);
+setTimeout(() => {
+  talkedRecently.delete(message.author.id);
+}, 300000);
+var originalt = originarray[Math.floor(Math.random() * originarray.length)];
+var embed = new Discord.RichEmbed()
+.setTitle(`Here is ur Origin account, sir.`)
+.setDescription(originalt)
+.setColor(c)
+message.author.sendMessage({embed});
+message.reply("check DMs.");
+break;	
+
+case "verify":
+if(message.guild.id !== "462635331712319498") return;
+ try {
+if(message.member.roles.some(r=>["🚫 | Member"].includes(r.name)))
+    return message.reply("You are already verified!");
+let user = message.author;
+let roleRemove = message.guild.roles.find("name", "Awaiting verify");
+let roleAdd = message.guild.roles.find("name", "🚫 | Member");
+message.member.removeRole(roleRemove.id);
+message.channel.send(`**${message.author.tag}** you have been succesfully verified!`);
+message.member.addRole(roleAdd.id);
+ } catch(err) {
+	 console.log(err)
+ }
+return;
+
 case "spotify":
-    if(message.guild.id == "431887036178497546") return message.channel.send("You cant use this bot on this server!");
-try {
+if(message.guild.id === "462635331712319498") return;
 if(talkedRecently.has(message.author.id)) {
 message.channel.send(`${message.author.tag}, You need to wait at least 5 minutes to type !spotify again!`);
   return;
@@ -119,48 +157,15 @@ setTimeout(() => {
   talkedRecently.delete(message.author.id);
 }, 300000);
 var salt = alt[Math.floor(Math.random() * textByLine.length)];
-message.author.send("Keep in mind that, we use ads to purchase new accounts \:)");
 var embed = new Discord.RichEmbed()
-.setTitle(`Here is your Spotify account.`)
+.setTitle(`Here is ur spotify alt, sir.`)
 .setDescription(salt)
-.setTimestamp()
-.setFooter("Thanks for using our Public GEN!", message.author.avatarURL)
 .setColor(c)
-message.author.send({embed});
-message.reply("check DMs. (make sure you dont have DMs disabled)");
-} catch(err) {
-  message.channel.send("You have DMs locked so I can't message you.")
-  return;
-}
-break;
-		    
-case "stats":
-try {
-let embed = new Discord.RichEmbed()
-.setAuthor("Bot Stats", message.author.avatarURL)
-.setDescription("I am in " + bot.guilds.size + " guilds with " + bot.users.size + " users")
-.setColor(message.guild.members.get(bot.user.id).displayHexColor)
-.setFooter("Random thing :^)", bot.user.avatarURL)
-message.channel.send({embed});
-} catch (err) {
-	console.log(err);
-	return;
-}
+message.author.sendMessage({embed});
+message.reply("check DMs.");
 break;
 
-case "invite":
-try {
-let embed = new Discord.RichEmbed()
-.setAuthor("Hey!! Invite me", bot.user.avatarURL)
-.setDescription("Make sure to invite me :)\n[Click me to invite.](https://discordapp.com/oauth2/authorize?client_id=457887453395681290&scope=bot&permissions=8)")
-.setColor(message.guild.members.get(bot.user.id).displayHexColor)
-message.channel.send({embed})
-} catch(err) {
-console.log(err);
-return;
-}
-break;
-    }
-    });
+};
+});
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.BOT_TOKEN);
